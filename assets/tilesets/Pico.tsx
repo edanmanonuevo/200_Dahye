86192859236eebc74df048169a6f73ac01f1d63f
edanmanonuevo <edan.m.anonuevo@student.uts.edu.au>
2025-08-03 15:31:02 +1000

    case sensitive

--- assets/tilesets/Pico.tsx
+++ assets/tilesets/Pico.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Pico" tilewidth="32" tileheight="32" tilecount="360" columns="24">
- <image source="pico.png" width="768" height="480"/>
+ <image source="Pico.png" width="768" height="480"/>
  <tile id="0">
   <properties>
    <property name="walkable" type="bool" value="true"/>
